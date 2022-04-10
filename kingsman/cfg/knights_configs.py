1c8c85f1dfbacf065fd30b20e545593d786de6f1
kimoyungh <eluster107@gmail.com>
2022-04-10 23:05:19 +0900

    add model

---
+++ kingsman/cfg/knights_configs.py
@@ -0,0 +1,20 @@
+"""
+    Knights Configs
+
+    @author: Younghyun Kim
+    Created on 2022.04.10
+"""
+TRADING_BERT_CONFIG = {
+    'factor_num': 24,
+    'asset_embeds_map_nlayers': 1,
+    'slope': 0.2,
+    'dropout': 0.1,
+    'max_len': 4096,
+    'd_model': 16,
+    'nhead': 4,
+    'nlayers': 4,
+    'activation': 'gelu',
+    'recon_map_nlayers': 1,
+    'comparing_map_nlayers': 1,
+    'overall_comparing_map_nlayers': 1,
+}
--- kingsman/knights.py
+++ kingsman/knights.py
@@ -4,10 +4,13 @@
     @author: Younghyun Kim
     Created on 2022.04.10
 """
+from dataclasses import dataclass
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from kingsman.cfg.knights_configs import TRADING_BERT_CONFIG
 from kingsman.layers import TransformerEnc, TransformerDec, Mapping
 
 
@@ -23,6 +26,9 @@ class TradingBERT(nn.Module):
             Args:
                 config: config dict
         """
+        if config is None:
+            config = TRADING_BERT_CONFIG
+
         super().__init__()
         self.config = config
         self.factor_num = config['factor_num']
@@ -90,3 +96,81 @@ class TradingBERT(nn.Module):
     @property
     def device(self):
         return next(self.parameters()).device
+
+    def forward(self, assets_in, seq_mask=False,
+            src_key_padding_mask=None):
+        """
+            Inference
+
+            Args:
+                stocks_in: feature data of multiple assets
+                    * dtype: torch.FloatTensor
+                    * shape: (batch_size, asset_num, seq_len, factor_num)
+            Return:
+                preds: predictions of each pretrain tasks
+                    * dtype: TradingBERTPreds
+                    'comps'
+                        * shape: (batch_size, seq_len, asset_num)
+                    'oc'
+                        * shape: (batch_size, 2)
+                    'recons'
+                        * shape: (batch_size, asset_num, seq_len, factor_num)
+                    'enc': None
+                        * shape: (batch_size, (asset_num+1)*seq_len, d_model)
+        """
+        batch_size = assets_in.shape[0]
+        asset_num = assets_in.shape[1]
+        seq_len = assets_in.shape[2]
+
+        # CLS Token
+        cls_init = self.cls_token(
+            torch.tensor([[0]]).to(self.device))
+        cls_init = cls_init.repeat(batch_size, 1, 1)
+        time_cls = self.time_encoding(
+            torch.tensor([[0]]).to(self.device))
+        time_cls = time_cls.repeat(batch_size, 1, 1)
+
+        cls_token = cls_init + time_cls
+
+        # Asset Embeddings
+        assets_init = self.asset_embeds(assets_in)
+        time_assets = self.time_encoding(
+            torch.arange(1, seq_len+1).to(self.device))
+        time_assets = time_assets.view(1, 1, seq_len, self.d_model)
+        time_assets = time_assets.repeat(batch_size, asset_num, 1, 1)
+
+        asset_embeds = assets_init + time_assets
+        asset_embeds =\
+            asset_embeds.permute(0, 2, 1, 3).contiguous().view(
+                batch_size, asset_num*seq_len, self.d_model)
+
+        assets = torch.cat((cls_token, asset_embeds), dim=1)
+
+        # Enter Transformer
+        assets = self.attn(assets, seq_mask=seq_mask,
+                        src_key_padding_mask=src_key_padding_mask)
+
+        # comp
+        comps = self.comparing_net(assets[:, 1:])
+        comps = comps.view(batch_size, seq_len, -1)
+
+        # oc
+        oc = self.overall_comparing_net(assets[:, 0])
+
+        # recon
+        recons = self.recon_net(assets[:, 1:])
+        recons = recons.view(batch_size, seq_len, asset_num, -1)
+        recons = recons.permute(0, 2, 1, 3).contiguous()
+
+        preds = TradingBERTPreds(
+            enc=assets, comps=comps, oc=oc, recons=recons)
+
+        return preds
+
+
+@dataclass
+class TradingBERTPreds:
+    enc: torch.FloatTensor
+    comps: torch.FloatTensor
+    oc: torch.FloatTensor
+    recons: torch.FloatTensor
